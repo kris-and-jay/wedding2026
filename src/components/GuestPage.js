275e550d6aa42063d44487173d9bd25d1f303684
Krisztian Ivan <krisztian.ivan@gmail.com>
2025-06-15 08:39:36 +0100

    added accomodation message to some guests

--- src/components/GuestPage.js
+++ src/components/GuestPage.js
@@ -9,7 +9,8 @@ const translations = {
     itinerary: "Itinerary",
     location: "Location",
     comingSoon: "Coming soon...",
-    locationText: "Our ceremony and reception will be held at Fattoria Terranova, a working farm and restaurant in the beautiful Sorrento hills, with wonderful views of the Bay of Naples, Vesuvius, and Capri.\n\nGuests are welcome to enjoy the swimming pool and garden areas. All the food served is grown on the farm, which also produces its own olive oil and limoncello from the region's famous Sorrento lemons.\n\nYou can find more details on their website: ",
+    locationText:
+      "Our ceremony and reception will be held at Fattoria Terranova, a working farm and restaurant in the beautiful Sorrento hills, with wonderful views of the Bay of Naples, Vesuvius, and Capri.\n\nGuests are welcome to enjoy the swimming pool and garden areas. All the food served is grown on the farm, which also produces its own olive oil and limoncello from the region's famous Sorrento lemons.\n\nYou can find more details on their website: ",
     websiteLink: "fattoriaterranova.it",
   },
   pl: {
@@ -19,7 +20,8 @@ const translations = {
     itinerary: "Plan wydarzeń",
     location: "Lokalizacja",
     comingSoon: "Wkrótce...",
-    locationText: "Nasza ceremonia i przyjęcie odbędą się w Fattoria Terranova – gospodarstwie z restauracją, położonym na pięknych wzgórzach Sorrento, z których roztaczają się wspaniałe widoki na Zatokę Neapolitańską, Wezuwiusza i Capri.\n\nGoście będą mogli korzystać z basenu oraz terenów ogrodowych. Wszystkie serwowane dania powstają z lokalnych produktów, a gospodarstwo wytwarza nawet własną oliwę z oliwek i limoncello ze słynnych cytryn z Sorrento!\n\nWięcej szczegółów znajdą na ich stronie internetowej: ",
+    locationText:
+      "Nasza ceremonia i przyjęcie odbędą się w Fattoria Terranova – gospodarstwie z restauracją, położonym na pięknych wzgórzach Sorrento, z których roztaczają się wspaniałe widoki na Zatokę Neapolitańską, Wezuwiusza i Capri.\n\nGoście będą mogli korzystać z basenu oraz terenów ogrodowych. Wszystkie serwowane dania powstają z lokalnych produktów, a gospodarstwo wytwarza nawet własną oliwę z oliwek i limoncello ze słynnych cytryn z Sorrento!\n\nWięcej szczegółów znajdą na ich stronie internetowej: ",
     websiteLink: "fattoriaterranova.it",
   },
   hu: {
@@ -29,9 +31,33 @@ const translations = {
     itinerary: "Program",
     location: "Helyszín",
     comingSoon: "Hamarosan...",
-    locationText: "A szertartást és a fogadást a Fattoria Terranovában tartjuk, egy étteremként is működő gazdaságban a gyönyörű Sorrento-i dombokon, ahonnan csodálatos kilátás nyílik a Nápolyi-öbölre, a Vezúvra és Capri szigetére.\n\nA vendégek rendelkezésére áll az úszómedence és a kert is. Minden felszolgált étel a farmon termesztett alapanyagokból készül, sőt, a gazdaság saját olívaolajat és limoncello-t is készít a híres Sorrento-i citromból!\n\nTovábbi részletek a weboldalukon: ",
+    locationText:
+      "A szertartást és a fogadást a Fattoria Terranovában tartjuk, egy étteremként is működő gazdaságban a gyönyörű Sorrento-i dombokon, ahonnan csodálatos kilátás nyílik a Nápolyi-öbölre, a Vezúvra és Capri szigetére.\n\nA vendégek rendelkezésére áll az úszómedence és a kert is. Minden felszolgált étel a farmon termesztett alapanyagokból készül, sőt, a gazdaság saját olívaolajat és limoncello-t is készít a híres Sorrento-i citromból!\n\nTovábbi részletek a weboldalukon: ",
     websiteLink: "fattoriaterranova.it",
   },
+};
+
+const customAccommodationMessages = {
+  IL2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  HG2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  AT2026B:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  SZ2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  ZZ2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  KR2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  GB2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  TM2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  ER2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
+  PT2026:
+    "Mélységes hálánk apró jeleként nagy örömmel látunk titeket vendégül az esküvő helyszínén és a hétvégi szállásotokat mi intézzük június 26-tól (péntek) június 28-ig (vasárnap). Csak azt szeretnénk, ha pihennétek, velünk ünnepelnétek, és csodás emlékeket szereznétek.\n\nA további részletekről (bejelentkezés, pontos szoba stb.) később küldünk tájékoztatást...",
 };
 
 const GuestPage = ({ language, guestCode }) => {
@@ -45,27 +71,45 @@ const GuestPage = ({ language, guestCode }) => {
         <h2>{translations[language].location}</h2>
         <div className="location-content">
           <div className="location-text">
-            {translations[language].locationText.split('\n\n').map((paragraph, index) => (
-              <p key={index}>
-                {paragraph}
-                {index === 2 && (
-                  <a 
-                    href="https://www.fattoriaterranova.it" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="website-link"
-                  >
-                    {translations[language].websiteLink}
-                  </a>
-                )}
-              </p>
-            ))}
+            {translations[language].locationText
+              .split("\n\n")
+              .map((paragraph, index) => (
+                <p key={index}>
+                  {paragraph}
+                  {index === 2 && (
+                    <a
+                      href="https://www.fattoriaterranova.it"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="website-link"
+                    >
+                      {translations[language].websiteLink}
+                    </a>
+                  )}
+                </p>
+              ))}
           </div>
           <div className="location-gallery">
-            <img src="/wedding2026/assets/fattoria/1.jpg" alt="Fattoria Terranova" className="gallery-image" />
-            <img src="/wedding2026/assets/fattoria/2.jpg" alt="Fattoria Terranova" className="gallery-image" />
-            <img src="/wedding2026/assets/fattoria/3.jpg" alt="Fattoria Terranova" className="gallery-image" />
-            <img src="/wedding2026/assets/fattoria/4.jpg" alt="Fattoria Terranova" className="gallery-image" />
+            <img
+              src="/wedding2026/assets/fattoria/1.jpg"
+              alt="Fattoria Terranova"
+              className="gallery-image"
+            />
+            <img
+              src="/wedding2026/assets/fattoria/2.jpg"
+              alt="Fattoria Terranova"
+              className="gallery-image"
+            />
+            <img
+              src="/wedding2026/assets/fattoria/3.jpg"
+              alt="Fattoria Terranova"
+              className="gallery-image"
+            />
+            <img
+              src="/wedding2026/assets/fattoria/4.jpg"
+              alt="Fattoria Terranova"
+              className="gallery-image"
+            />
           </div>
         </div>
       </div>
@@ -75,7 +119,13 @@ const GuestPage = ({ language, guestCode }) => {
       </div>
       <div className="section">
         <h2>{translations[language].accommodation}</h2>
-        <p>{translations[language].comingSoon}</p>
+        {customAccommodationMessages[guestCode] ? (
+          <p style={{ whiteSpace: "pre-line" }}>
+            {customAccommodationMessages[guestCode]}
+          </p>
+        ) : (
+          <p>{translations[language].comingSoon}</p>
+        )}
       </div>
       <div className="section">
         <h2>{translations[language].itinerary}</h2>
