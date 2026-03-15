5fd2535da1aaa48a7aafcbf7dbad5adadeb34e8c
Krisztian Ivan <krisztianivan@Krisztians-MacBook-Pro.local>
2026-03-15 19:23:24 +0000

    updated Kasia RSVP

--- src/components/RSVPForm.js
+++ src/components/RSVPForm.js
@@ -156,6 +156,11 @@ const RSVPForm = ({ language, guestCode }) => {
       pl: "Bardzo dziękujemy za potwierdzenie obecności! Cieszymy się niezmiernie, że będziesz z nami w tym wyjątkowym dniu!",
       hu: "Köszönjük, hogy visszajeleztél! Nagyon örülünk, hogy velünk ünnepeltek ezen a különleges napon!",
     },
+    KS2026: {
+      en: "Thank you so much for confirming your attendance! We are super excited to have you on our special day!",
+      pl: "Bardzo dziękujemy za potwierdzenie obecności! Cieszymy się niezmiernie, że będziesz z nami w tym wyjątkowym dniu!",
+      hu: "Köszönjük, hogy visszajeleztél! Nagyon örülünk, hogy velünk ünnepeltek ezen a különleges napon!",
+    },
     // Add more guest codes here as needed
   };
 
